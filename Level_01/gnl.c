449161a9a6847ea3086e1534841339b1ff03e8d3
Vavongg <vavong@vavong-laptop.home>
2025-08-18 17:50:28 +0200

    Update

--- Level_01/gnl.c
+++ Level_01/gnl.c
@@ -1,77 +1,82 @@
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <string.h>
 
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 1024
 #endif
 
-//1ere version de GNL
-
-static char *ft_strjoin_free(char *s1, char *s2, int len2)
+int	ft_strlen(char *str)
 {
-    int len1 = 0;
-    while (s1 && s1[len1])
-        len1++;
-    char *res = malloc(len1 + len2 + 1);
-    if (!res)
-        return NULL;
-    for (int i = 0; i < len1; i++)
-        res[i] = s1[i];
-    for (int j = 0; j < len2; j++)
-        res[len1 + j] = s2[j];
-    res[len1 + len2] = '\0';
-    free(s1);
-    return res;
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
 }
 
-char *get_next_line(int fd)
+char *ft_strdup(char *str)
 {
-    static char buffer[BUFFER_SIZE];
-    static int b_read = 0;
-    static int pos = 0;
-    char *line = NULL;
-    int i;
-
-    if (fd < 0 || BUFFER_SIZE <= 0)
+    int i = 0;
+    char *new = malloc(sizeof(char) * (ft_strlen(str) + 1));
+    if (!new)
         return NULL;
-
-    while (1)
+    while (str[i])
     {
-        if (pos >= b_read)
-        {
-            b_read = read(fd, buffer, BUFFER_SIZE);
-            pos = 0;
-            if (b_read <= 0)
-                return line;
-        }
-        i = pos;
-        while (i < b_read && buffer[i] != '\n')
-            i++;
-        line = ft_strjoin_free(line, buffer + pos, i - pos);
-        pos = i;
-        if (pos < b_read && buffer[pos] == '\n')
-        {
-            line = ft_strjoin_free(line, "\n", 1);
-            pos++;
-            return line;
-        }
+        new[i] = str[i];
+        i++;
     }
+    new[i] = '\0';
+    return new;
 }
 
-int main(int ac, char **av)
+char *gnl(int fd)
 {
-    if (ac < 2)
-        return 1;
-    int fd = open(av[1], O_RDONLY);
-    char *line;
-    while ((line = get_next_line(fd)))
-    {
-        printf("%s", line);
-        free(line);
-    }
-    close(fd);
-    return 0;
+	static char buffer[BUFFER_SIZE];
+	static int pos = 0;
+	static int b_read = 0;
+	char line[70000];
+	int i = 0;
+
+	if(fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
+	while (1)
+	{
+		if (pos >= b_read)
+		{
+			b_read = read(fd, buffer, BUFFER_SIZE);
+			pos = 0;
+			if (b_read <= 0)
+				break;
+		}
+		line[i++] = buffer[pos++];
+		if (line[i - 1] == '\n')
+			break;
+	}
+	if (i == 0)
+		return NULL;
+	line[i] = '\0';
+	return (ft_strdup(line));
+}
+
+int main (int ac, char **av)
+{
+	int fd;
+	char *str;
+	(void)ac;
+
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+	{
+		perror("open");
+		return (1);
+	}
+	while ((str = gnl(fd)) != NULL)
+	{
+		printf("%s", str);
+		free(str);
+	}
+	close(fd);
+	return 0;
 }
